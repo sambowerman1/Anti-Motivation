3e608f52ed62578a5369cf5df323f43cf356e8de
Sam Bowerman <119436495+sambowerman1@users.noreply.github.com>
2023-12-24 20:33:18 -0600

    Initial Commit

---
+++ A10WIDGET/A10WIDGETApp.swift
@@ -0,0 +1,17 @@
+//
+//  A10WIDGETApp.swift
+//  A10WIDGET
+//
+//  Created by Sam Bowerman on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct A10WIDGETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
